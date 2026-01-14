2df1f7ffd300d56b4e8e87718f53742011f2b870
HEMA POLUMATI <hema.p23@iiits.in>
2026-01-14 10:36:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9125365998-d44e7",
+  "appId": "1:819916151655:web:eeba229dbac77aaf6cedc2",
+  "apiKey": "AIzaSyC-fjSZmo95f-HLPEJ02sWG2xQO7qN77TE",
+  "authDomain": "studio-9125365998-d44e7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "819916151655"
+};
